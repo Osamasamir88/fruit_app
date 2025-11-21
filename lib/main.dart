3b5e5b05fed2f785643440e52dd01e3ac3c10442
Osama <samirosama127@gmail.com>
2025-11-21 04:27:47 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitApp());
+}
+
+class FruitApp extends StatelessWidget {
+  const FruitApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
